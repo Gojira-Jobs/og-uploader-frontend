34294dc910a83c496a4756aae7bda37f0facd6b3
Rahul Kumar <rahul.x@venturepact.com>
2017-02-27 15:17:19 +0530

    Bugs fixed and some animations tried

--- src/app/og-uploader/image-upload/image-upload.component.ts
+++ src/app/og-uploader/image-upload/image-upload.component.ts
@@ -2,8 +2,6 @@ import {Component, Input, Output, EventEmitter, ViewChild} from '@angular/core';
 import {ImageService, Header} from "../image.service";
 declare var $;
 class FileHolder {
-  public serverResponse: any;
-  public pending: boolean = false;
   constructor(private src: string, public file: File) { }
 }
 
@@ -15,43 +13,31 @@ class FileHolder {
 export class ImageUploadComponent {
   public visible:boolean=false;
   private visibleAnimate = false;
-  max: number = 1;
   url: string="http://97ed60c2.ngrok.io";
-  headers: Header[];
-  @Input() acceptTypes : string[];
-  @Input() styling: string='btn btn-primary';
-  @Input() quality: string='65';
-  @ViewChild('input') input;
-  @ViewChild('url') urlField;
-  @Output()
-  private isPending: boolean;
-  @Output()
-  private onFileUploadFinish: EventEmitter<FileHolder> = new EventEmitter<FileHolder>();
+  headers: Header[];  
   private errMsg: string;
   private errMsgShow: boolean;
   private files: FileHolder;
   private loadingImg: string="../../../assets/transfer.gif";
-  private finishImg: string="../../../assets/double.png"
-  private fileCounter: number = 0;
+  private finishImg: string="../../../assets/double.png";
   private rotatingGif: string;
   private pendingFilesCounter: number = 0;
-
   private isFileOver:boolean = false;
-
+  private isPending: boolean;
+  private uploadButtonCaption: string = "Select File";
+  private dropBoxMessage: string = "Drop your file here!";
   @Input()
-  private buttonCaption: string = "Select Images";
-  @Input()
-  private uploadButtonCaption: string = "Select Images";
-
-  @Input()
-  private dropBoxMessage: string = "Drop your images here!";
-
+  private buttonCaption: string = "Upload";
+  @Input() acceptTypes : string[];
+  @Input() styling: string;
+  @Input() quality: string='65';
+  @ViewChild('url') urlField;
+  @Output()
+  private onFileUploadFinish: EventEmitter<FileHolder> = new EventEmitter<FileHolder>();
+  
   constructor(private imageService: ImageService) { }
 
   ngOnInit() {
-    if(this.max==1){
-      this.input.nativeElement.multiple = false;
-    }
     this.isPending=false;
     this.imageService.setUrl(this.url);
     this.imageService.setQuality(this.quality);
@@ -59,36 +45,21 @@ export class ImageUploadComponent {
 
   fileChange(files) {
     this.errMsgShow=false;
-    if (this.url) {
-      this.isPending=true;
-      this.rotatingGif=this.loadingImg;
-    }
-
-
-   let reader = new FileReader();
+    this.isPending=true;
+    this.rotatingGif=this.loadingImg;
+    let reader = new FileReader();
       reader.addEventListener('load', (event: any) => {
         let fileHolder: FileHolder = new FileHolder(event.target.result, files[0]);
-
-        fileHolder.serverResponse = `good boy`;
-
-        this.uploadSingleFile(fileHolder);
-
+        this.uploadFile(fileHolder);
         this.files=fileHolder;
-
       }, false);
-
-
       reader.readAsDataURL(files[0]);
   }
 
-  private uploadSingleFile(fileHolder: FileHolder) {
+  private uploadFile(fileHolder: FileHolder) {
     if (this.url) {
-      fileHolder.pending = true;
-
       this.imageService.postImage(fileHolder.file, this.headers).subscribe(response => {
-        fileHolder.serverResponse = response;
         this.onFileUploadFinish.emit(response.InkBlob);
-        fileHolder.pending = false;
         this.rotatingGif=this.finishImg;
         setTimeout(() => this.hide(), 1500);
       },err=>{
@@ -105,11 +76,6 @@ export class ImageUploadComponent {
     this.isFileOver = isOver;
   }
 
-  modal()
-  {
-    this.show();
-   
-  }
   public show(): void {
         this.visible = true;
         setTimeout(() => this.visibleAnimate = true);
@@ -126,6 +92,7 @@ export class ImageUploadComponent {
   send(val:string){
     this.errMsgShow=false;
     this.isPending=true;
+    this.rotatingGif=this.loadingImg;
     this.imageService.linkupload(val,this.headers).subscribe(res=>
     {
       this.onFileUploadFinish.emit(res.InkBlob);
